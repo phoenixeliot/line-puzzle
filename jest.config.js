6242d203ebafe89393f307a5101bfd85492f54fd
Phoenix Eliot <phoenixjeliot@gmail.com>
2021-12-20 22:21:19 -0500

    Add SWC for Jest speedup

--- jest.config.js
+++ jest.config.js
@@ -2,16 +2,12 @@
 module.exports = {
   preset: "ts-jest",
   transform: {
-    "^.+\\.m?jsx?$": "babel-jest",
-    "^.+\\.tsx?$": "ts-jest"
+    "^.+\\.(t|j)sx?$": "@swc/jest",
   },
   testEnvironment: "node",
   // testRegex: ["/tests/.*test.m?js"],
   // testMatch: ["**/?(*.)+(spec|test).?(m)[tj]s(x)?", "**/*.mjs"],
-  testMatch: [
-    "**/__tests__/**/*.[jt]s?(x)",
-    "**/?(*.)+(spec|test).?(m)[jt]s?(x)",
-  ],
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).?(m)[jt]s?(x)"],
   moduleFileExtensions: ["js", "jsx", "ts", "mjs"],
   setupFiles: ["./tests/setupTests.ts"],
 };
